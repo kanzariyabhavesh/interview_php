fb937bf458c24af2c6b6ac74833cb03f0811ade1
Sandip <sandip@binarystorm.in>
2024-05-11 17:36:53 +0530

    test

--- interview _program.php/frist.php
+++ interview _program.php/frist.php
@@ -1,22 +1,22 @@
 <?php
 
-class a
-{
-    function  x(int $a){
+// class a
+// {
+//     function  x(int $a){
 
-        if ($a % 3 == 0 && $a % 5 == 0) {
-            echo "it is 3 and 5";
-        }
-        elseif ($a % 3 == 0){
-            echo "it is 3";
-        }
-        elseif ($a % 5 == 0){
-            echo "it is 5";
-        }
+//         if ($a % 3 == 0 && $a % 5 == 0) {
+//             echo "it is 3 and 5";
+//         }
+//         elseif ($a % 3 == 0){
+//             echo "it is 3";
+//         }
+//         elseif ($a % 5 == 0){
+//             echo "it is 5";
+//         }
 
-    }
-}
+//     }
+// }
  
-$abc=new a();
-$abc->x(17);
+// $abc=new a();
+// $abc->x(17);
 ?>
